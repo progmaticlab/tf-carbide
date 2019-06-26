30f4e7e15180acc0fadbc0f28ee004520b663f28
OlegBravo <oleg.bravo.ru@gmail.com>
2019-06-26 13:08:36 +0300

    make output wide

--- sandbox/scripts/get_pods.sh
+++ sandbox/scripts/get_pods.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash -ex
 source /opt/sandbox/scripts/environment
 cd /opt/sandbox/scripts
-ssh -v -i id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -t centos@$CONTROLLER "sudo kubectl get pods --all-namespaces"
+ssh -v -i id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -t centos@$CONTROLLER "sudo kubectl get pods --all-namespaces -o wide"
